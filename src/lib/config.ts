26d25aa988c922fc1e02173c45be2babc972b8f6
Collin Lowenburg <colin@lowenberg.org>
2025-12-02 22:50:36 -0800

    Fix missing floor imports causing build error

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -5,6 +5,7 @@ import { Floor1 } from '@/components/floor-svgs/floor-1';
 import { Floor2 } from '@/components/floor-svgs/floor-2';
 import { Floor3 } from '@/components/floor-svgs/floor-3';
 import { Floor4 } from '@/components/floor-svgs/floor-4';
+import { Floor5 } from '@/components/floor-svgs/floor-5';
 import { Floor6 } from '@/components/floor-svgs/floor-6';
 import { Floor7 } from '@/components/floor-svgs/floor-7';
 import { Floor8 } from '@/components/floor-svgs/floor-8';
@@ -24,6 +25,7 @@ import * as floor2Data from '@/components/floor-svgs/floor-2';
 import * as floor3Data from '@/components/floor-svgs/floor-3';
 import * as floor4Data from '@/components/floor-svgs/floor-4';
 import * as floor5Data from '@/components/floor-svgs/floor-5';
+import * as floor6Data from '@/components/floor-svgs/floor-6';
 import * as floor7Data from '@/components/floor-svgs/floor-7';
 import * as floor8Data from '@/components/floor-svgs/floor-8';
 import * as floor9Data from '@/components/floor-svgs/floor-9';
@@ -42,6 +44,7 @@ const floorModules = [
   floor2Data,
   floor3Data,
   floor4Data,
+  floor5Data,
   floor6Data,
   floor7Data,
   floor8Data,
@@ -53,6 +56,7 @@ const floorModules = [
   floor14Data,
   floor15Data,
   floor16Data,
+  floor17Data,
 ];
 
 export const allFloors: Floor[] = floorModules.map(module => ({
@@ -68,6 +72,7 @@ export const floorComponentMap: { [key: string]: React.ComponentType<any> } = {
     '3': Floor3,
     '4': Floor4,
     '5': Floor5,
+    '6': Floor6,
     '7': Floor7,
     '8': Floor8,
     '9': Floor9,
@@ -77,4 +82,5 @@ export const floorComponentMap: { [key: string]: React.ComponentType<any> } = {
     '14': Floor14,
     '15': Floor15,
     '16': Floor16,
+    '17': Floor17,
 };
